b7e5012fb2896b222d64ae0fbe2037c4e8333f15
Stéphane BOURET <sbouret@orange.fr>
2024-05-09 17:35:05 +0200

    Maj d'une version de migration onsolète

--- migrations/Version20240420135757.php
+++ migrations/Version20240420135757.php
@@ -22,7 +22,7 @@ final class Version20240420135757 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE borrowing (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, borrowing_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', total INT NOT NULL, INDEX IDX_226E5897A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE borrowing ADD CONSTRAINT FK_226E5897A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('UPDATE purchase SET purchased_at = NOW()');
+        $this->addSql('UPDATE borrowing SET borrowing_at = NOW()');
     }
 
     public function down(Schema $schema): void
