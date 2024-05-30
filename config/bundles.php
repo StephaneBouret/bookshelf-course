1c5942839e51a683642983ff2fb1771ca5409e7f
Stéphane BOURET <sbouret@orange.fr>
2024-05-30 16:22:52 +0200

    Mise en place de Webpack Encore

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
